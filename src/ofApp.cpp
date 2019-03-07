302a958de1f460e1ad31d99c9c0de0822063468d
jenny <jennifer.farman@gmail.com>
2019-03-07 17:51:30 -0500

    First commit: Initialized repo; Created basic tool for drawing 45 degree triangles given two points (which can be changed via mouse input); Added UI / readout for adjusting a perpendicular line that cuts across the bisecting line (interpolation along bisecting line can also be adjusted via mouse input)

---
+++ src/ofApp.cpp
@@ -0,0 +1,207 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	bool shouldUpdateTriangle = false;
+	
+	if (isDraggingEndPoint) {
+		endPoint = ofPoint(mouseX, mouseY);
+		shouldUpdateTriangle = true;
+	}
+	else if (isDraggingStartPoint) {
+		startPoint = ofPoint(mouseX, mouseY);
+		shouldUpdateTriangle = true;
+	}
+	if (shouldUpdateTriangle || hasWindowResized) {
+		bisectingLine.clear();
+		bisectingLine.addVertex(startPoint.x, startPoint.y);
+		bisectingLine.addVertex(endPoint.x, endPoint.y);
+		
+		if (isBisectingLineLerpPointActive) {
+			bisectingLineLerpPoint = (ofPoint)bisectingLine.getPointAtIndexInterpolated(currentLerpIndex);
+			float lerpPointDistance = bisectingLineLerpPoint.distance(startPoint);
+			float triangleHeight = ofDist(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
+			currentLerpIndex = lerpPointDistance / triangleHeight;
+		}
+
+		lineSlope = (endPoint.x - startPoint.x) / (endPoint.y - startPoint.y);
+		lineIntercept = endPoint.x - (lineSlope*endPoint.y);
+
+		if (hasWindowResized) hasWindowResized = false;
+	}
+	else if (isDraggingBisectingLineLerpPoint) {
+		float max = MAX(startPoint.y, endPoint.y);
+		float min = MIN(startPoint.y, endPoint.y);
+		float y = ofClamp(mouseY, min, max);
+		ofPoint linePoint = ofPoint(lineSlope * y + lineIntercept, y);
+		bisectingLineLerpPoint = linePoint;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	ofPushMatrix();
+
+	ofSetColor(0, 90, 60);
+	ofSetLineWidth(LINE_WIDTH);
+
+	float halfVertex = 45.0 / 2.0f;
+	float triangleHeight = ofDist(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
+	float triangleLength = triangleHeight / cos(ofDegToRad(halfVertex));
+
+	ofVec2f directionVector = ofVec2f(endPoint.x - startPoint.x, endPoint.y - startPoint.y);
+	ofVec2f normalized = directionVector.getNormalized();
+	int angle = normalized.angle(ofVec2f(0, -1));
+
+	double addRadians = ofDegToRad(angle + halfVertex);
+	double subRadians = ofDegToRad(angle - halfVertex);
+
+	// Adapted from https://math.stackexchange.com/q/1101779
+	ofPoint vertexA = ofPoint(startPoint.x - (triangleLength * sin(addRadians)), startPoint.y - (triangleLength * cos(addRadians)));
+	ofPoint vertexB = ofPoint(startPoint.x - (triangleLength * sin(subRadians)), startPoint.y - (triangleLength * cos(subRadians)));
+
+	ofNoFill();
+	ofSetColor(ofColor::mediumPurple);
+	ofDrawTriangle(startPoint.x, startPoint.y, vertexA.x, vertexA.y, vertexB.x, vertexB.y);
+	
+	ofFill();
+	
+	ofSetColor(ofColor::magenta);
+	ofDrawCircle(startPoint, POINT_RADIUS);
+	ofDrawCircle(endPoint, POINT_RADIUS);
+
+	bisectingLine.draw();
+
+	string currentLerpIndexString = "n/a";
+
+	if (isBisectingLineLerpPointActive) {
+		float lerpPointDistance = bisectingLineLerpPoint.distance(startPoint);
+		currentLerpIndex = lerpPointDistance / triangleHeight;
+		currentLerpIndexString = ofToString(currentLerpIndex, 4);
+
+		ofSetColor(ofColor::orange);
+		ofDrawCircle(bisectingLineLerpPoint, POINT_RADIUS);
+
+		ofPolyline lineA; // todo: draw triangle using these lines instead of ofDrawTriangle()
+		lineA.addVertex(startPoint.x, startPoint.y);
+		lineA.addVertex(vertexA);
+		ofPoint interpolatedLineAPoint = (ofPoint)lineA.getPointAtIndexInterpolated(currentLerpIndex);
+
+		ofPolyline lineB;
+		lineB.addVertex(startPoint.x, startPoint.y);
+		lineB.addVertex(vertexB);
+		ofPoint interpolatedLineBPoint = (ofPoint)lineB.getPointAtIndexInterpolated(currentLerpIndex);
+
+		ofPolyline lineAB;
+		lineAB.addVertex(interpolatedLineAPoint);
+		lineAB.addVertex(interpolatedLineBPoint);
+		lineAB.draw();
+
+		ofSetColor(ofColor::lightGreen);
+		ofDrawCircle(interpolatedLineAPoint, POINT_RADIUS);
+		ofDrawCircle(interpolatedLineBPoint, POINT_RADIUS);
+	}
+
+	ofSetColor(ofColor::white);
+	ofDrawBitmapString("interpolation index along bisection of triangle = " + currentLerpIndexString, 10, 25);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+	ofPoint clickedPoint = ofPoint(x, y);
+	ofPoint linePoint = ofPoint(lineSlope * y + lineIntercept, y);
+	// allow line clicks within a reasonable buffer distance
+	float maxDistance = LINE_WIDTH + LINE_CLICK_BUFFER;
+	if (endPoint.distance(clickedPoint) < POINT_RADIUS) {
+		isDraggingEndPoint = true;
+	}
+	else if (startPoint.distance(clickedPoint) < POINT_RADIUS) {
+		isDraggingStartPoint = true;
+	}
+	else if (linePoint.distance(clickedPoint) < maxDistance) {
+		bisectingLineLerpPoint = linePoint;
+		isBisectingLineLerpPointActive = true;
+		isDraggingBisectingLineLerpPoint = true;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+	isDraggingEndPoint = false;
+	isDraggingStartPoint = false;
+	isDraggingBisectingLineLerpPoint = false;
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+	float widthRatio = (float)w / windowWidth;
+	float heightRatio = (float)h / windowHeight;
+	
+	startPoint = ofPoint(startPoint.x * widthRatio, startPoint.y * heightRatio);
+	endPoint = ofPoint(endPoint.x * widthRatio, endPoint.y * heightRatio);
+
+	windowWidth = w;
+	windowHeight = h;
+
+	hasWindowResized = true;
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+
+
+//--------------------------------------------------------------  
+void ofApp::setup() {
+
+	ofBackground(ofColor::black);
+
+	windowWidth = ofGetWindowWidth();
+	windowHeight = ofGetWindowHeight();
+
+	// Arbitrary initial position for triangle
+	startPoint = ofPoint(windowWidth/8.0f, windowHeight/4.0f);
+	endPoint = ofPoint(windowWidth/2.0, windowHeight/1.5f);
+
+	bisectingLine.addVertex(startPoint.x, startPoint.y);
+	bisectingLine.addVertex(endPoint.x, endPoint.y);
+
+	ofSetFullscreen(false);
+}
---
+++ src/ofApp.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "ofMain.h"
+#include <math.h>.
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+	protected:
+		ofPoint startPoint;
+		ofPoint endPoint;
+		ofPolyline bisectingLine;
+		
+		bool isDraggingEndPoint;
+		bool isDraggingStartPoint;
+
+		float lineSlope;
+		float lineIntercept;
+
+		ofPoint bisectingLineLerpPoint;
+		bool isBisectingLineLerpPointActive;
+		bool isDraggingBisectingLineLerpPoint;
+
+		float currentLerpIndex;
+
+		const float POINT_RADIUS = 7.0f;
+		const float LINE_WIDTH = 3.0f;
+		const float LINE_CLICK_BUFFER = 10.0f;
+
+		float windowWidth;
+		float windowHeight;
+
+		bool hasWindowResized;
+};
